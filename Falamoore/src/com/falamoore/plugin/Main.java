b903b6a97d3802112a64a9906d70e0053c5ba9f7
stelar7 <s.larssen@hotmail.com>
2012-09-03 18:18:50 +0200

    Initial upload

---
+++ Falamoore/src/com/falamoore/plugin/Main.java
@@ -0,0 +1,14 @@
+package com.falamoore.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public void onEnable() {
+    }
+}
